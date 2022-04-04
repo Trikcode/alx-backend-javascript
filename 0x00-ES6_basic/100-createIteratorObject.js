607b344e2c8825fc94016c3029f6b60b31081570
Trikcode <ayesiganobert@gmail.com>
2022-04-04 20:09:59 +0300

    iterable

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,9 @@
+export default function createIteratorObject(report) {
+   let employee = [];
+   for (let [depts, emplys] of Object.entries(report.allEmployees)){
+        for (let emp of emplys){
+            employee.push(emp);
+        }
+   }
+   return employee;
+}
