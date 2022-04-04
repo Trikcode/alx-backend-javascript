461278598bf9bed7746f24f223aa2dee69eb0246
Trikcode <ayesiganobert@gmail.com>
2022-04-04 19:57:21 +0300

    blocked

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,10 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
+    /* eslint-disable no-unused-vars */
     const task = true;
     const task2 = false;
+    /* eslint-enable no-unused-vars */
   }
 
   return [task, task2];
