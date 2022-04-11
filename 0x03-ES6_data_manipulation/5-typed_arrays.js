56195ef4a27f2b0e735b0ed4e9bf67276156e2cb
Nobert Patrick <ayesiganobert@gmail.com>
2022-04-11 21:35:34 +0300

    Delete 5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++
@@ -1,9 +0,0 @@
-const createInt8TypedArray = (length, position, value) => {
-  const buffer = new ArrayBuffer(length);
-  const int8Array = new Int8Array(buffer);
-  if (position > int8Array.length) throw new Error('Position outside range');
-  int8Array[position] = value;
-  return new DataView(buffer);
-};
-
-export default createInt8TypedArray;
