3be080bc7f7ba412c6c0d2b172230d4fcdcbf2cb
Trikcode <ayesiganobert@gmail.com>
2022-04-11 21:25:56 +0300

    student upgrade

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,22 @@
+function updateStudentGradeByCity(students, city, newGrades) {
+  const studentsInCity = students.filter(
+    (student) => student.location === city
+  );
+  return studentsInCity.map((student) => {
+    const filteredGrades = newGrades.filter(
+      (grade) => grade.studentId === student.id
+    );
+    if (filteredGrades.length > 0) {
+      return {
+        ...student,
+        grade: filteredGrades[0].grade,
+      };
+    }
+    return {
+      ...student,
+      grade: "N/A",
+    };
+  });
+}
+
+export default updateStudentGradeByCity;
